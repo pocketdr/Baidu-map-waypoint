dc1bc30f45ebd241b36c920d035f26987405c167
pocketdr <pocketz1783@gmail.com>
2021-02-18 02:39:12 +0800

    json construction

---
+++ src/main/java/com/pocketz/project/PathPlan.java
@@ -0,0 +1,38 @@
+package com.pocketz.project;
+
+import java.util.List;
+
+public class PathPlan {
+	public Result result;
+}
+
+class Result{
+	public List<Route> routes;
+}
+
+class Route{
+	public int distance;
+	public int duration;
+	
+	public List<Step> steps;
+}
+
+class Step{
+	public int distance;
+	public int duration;
+	public String instructions;
+	public String name;
+	
+	public String path;
+	public StepOriginLocation stepOriginLocation;
+	public StepDestinationLocation stepDestinationLocation;
+	
+}
+class StepOriginLocation{
+	public double lng;
+	public double lat;
+}
+class StepDestinationLocation{
+	public double lng;
+	public double lat;
+}
