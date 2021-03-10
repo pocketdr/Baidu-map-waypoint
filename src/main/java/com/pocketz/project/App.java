49b2e496a30201ac7f6baa503edada59b78dc17c
pocketdr <pocketz1783@gmail.com>
2021-03-10 13:22:36 +0800

    add GCJ to WSG84 and change packages

--- src/main/java/com/pocketz/project/App.java
+++ src/main/java/com/pocketz/project/App.java
@@ -1,45 +1,43 @@
 package com.pocketz.project;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpClient.Version;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Hello world!
  *
  */
 public class App {
-	static String URI = "http://api.map.baidu.com/direction/v2/riding?origin=39.806717,116.48806&destination=39.80957,116.49471&ak=9gXOVbtuir0Di1cw75BEhHtxIHkuQHRH";
+	static String URI1 = "http://api.map.baidu.com/direction/v2/riding?origin=39.806717,116.48806&destination=39.80957,116.49471&ak=9gXOVbtuir0Di1cw75BEhHtxIHkuQHRH";
+	static String URI2 = "http://api.map.baidu.com/place/v2/search?query=公交车站&location=38.309737,116.858596&radius=10000000&radius_limit=true&output=json&page_size=20&page_num=4&ret_coordtype=gcj02ll&ak=9gXOVbtuir0Di1cw75BEhHtxIHkuQHRH";
 
 	static String ak = "&ak=9gXOVbtuir0Di1cw75BEhHtxIHkuQHRH"; 
+	
+	//116.844079,38.307981
+//	116.836093,38.298948
+//	116.858596,38.309737
 	public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
-		WaypointsInput waypointsInput =new WaypointsInput();
+//		WaypointsInput waypointsInput =new WaypointsInput();
 		WaypointHandler waypointHandler = new WaypointHandler();
+		APIHandler apiHandler = new APIHandler();
 
-		waypointHandler.setURI(URI);
-		String[] waypointStrings = waypointHandler.getWaypoints();
+		apiHandler.setURI(URI2);
+//		String[] waypointStrings = waypointHandler.getWaypoints(apiHandler.sendRequest());
 		
-		for (String string : waypointStrings) {
-			System.out.println(string);
+		String locations = apiHandler.sendRequest();
+//		System.out.println(locations);
+		
+		List<String> locationList = waypointHandler.getLocations(locations);
+		String[] bfSArray = null;
+		for (String s : locationList) {
+			bfSArray = s.split(",");
+			double[] points = CoordinateTransformUtil.gcj02towgs84(Double.parseDouble(bfSArray[0]), Double.parseDouble(bfSArray[1]));
+			System.out.println(points[0]+","+points[1]);
 		}
+//		for (String string : waypointStrings) {
+//			System.out.println(string);
+//		}
 	}
 }
+
