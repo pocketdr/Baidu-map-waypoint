760e887cdc5a00975d19285c7dff006520b0da9b
pocketdr <pocketz1783@gmail.com>
2021-03-10 14:24:59 +0800

    delete personal info

--- src/main/java/com/pocketz/project/App.java
+++ src/main/java/com/pocketz/project/App.java
@@ -4,19 +4,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
 
-/**
- * Hello world!
- *
- */
 public class App {
-//	static String URI1 = "http://api.map.baidu.com/direction/v2/riding?origin=39.806717,116.48806&destination=39.80957,116.49471&ak=9gXOVbtuir0Di1cw75BEhHtxIHkuQHRH";
-//	static String URI2 = "http://api.map.baidu.com/place/v2/search?query=公交车站&location=38.309737,116.858596&radius=10000000&radius_limit=true&output=json&page_size=20&page_num=4&ret_coordtype=gcj02ll&ak=9gXOVbtuir0Di1cw75BEhHtxIHkuQHRH";
-//
-//	static String ak = "&ak=9gXOVbtuir0Di1cw75BEhHtxIHkuQHRH"; 
-//	
-	//116.844079,38.307981
-//	116.836093,38.298948
-//	116.858596,38.309737
 	public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
 //		WaypointsInput waypointsInput =new WaypointsInput();
 		WaypointHandler waypointHandler = new WaypointHandler();
--- src/main/java/com/pocketz/project/URICreator.java
+++ src/main/java/com/pocketz/project/URICreator.java
@@ -1,22 +1,8 @@
 package com.pocketz.project;
 
 public class URICreator {
-//	static String URI1 = "http://api.map.baidu.com/direction/v2/riding"
-//			+ "?origin=39.806717,116.48806"
-//			+ "&destination=39.80957,116.49471"
-//			+ "&ak=9gXOVbtuir0Di1cw75BEhHtxIHkuQHRH";
-//	static String URI2 = "http://api.map.baidu.com/place/v2/search"
-//			+ "?query=公交车站"
-//			+ "&location=38.298948,116.836093"
-//			+ "&radius=3000"
-//			+ "&radius_limit=false"
-//			+ "&output=json"
-//			+ "&page_size=20"
-//			+ "&page_num=3"
-//			+ "&coord_type=1"
-//			+ "&ak=9gXOVbtuir0Di1cw75BEhHtxIHkuQHRH";
-
-	final static String AK = "&ak=9gXOVbtuir0Di1cw75BEhHtxIHkuQHRH"; 
+ 
+	final static String AK = ""; 
 	
 	final static String BASE_URI = "http://api.map.baidu.com/";
 	
